76e86abd2ea21e6fb8e13cc65cfc97f278710098
Luis Rodrigues <luis.guilherme@mblabs.com.br>
2019-11-12 22:52:09 -0300

    Primeiro commit da mulecada

---
+++ app/src/main/java/com/example/ticketou_android/view/adapter/EventListRecyclerAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.ticketou_android.view.adapter
+
+import android.widget.AbsListView
+import androidx.recyclerview.widget.RecyclerView
+
+class EventListRecyclerAdapter: RecyclerView.Adapter<> {
+}
